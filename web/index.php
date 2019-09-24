443f904f809f3cb6eafc5727de13bf6c4cbb6a5a
Lucas Wargha <war18001@byui.edu>
2019-09-24 14:41:55 -0600

    Test PHP

--- web/index.php
+++ web/index.php
@@ -1,5 +1,18 @@
-<?php
-
-  phpinfo();
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+   <form class="login container col">
+       <span>Login: </span>
+       <input type="text">
+       <span>Password: </span>
+       <input type="password">
+   </form>
+</body>
+</html>
