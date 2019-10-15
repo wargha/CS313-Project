f3303fd3f223e4a823293af3b7127d66b7326305
Lucas Wargha <war18001@byui.edu>
2019-10-15 16:07:24 -0600

    test

--- web/week-5/team.php
+++ web/week-5/team.php
@@ -12,7 +12,7 @@ $db = get_db();
     <?php 
     // echo "HERE!!!";
  
-    $sql = "SELECT * FROM TEST";
+    $sql = "SELECT * FROM SCRIPTURE";
     echo "HERE!!!1";
     $rs = pg_query($db, $sql) or die("Cannot execute query: $sql\n");
     echo "HERE!!!2";
