357fe4e2b4d1f4ec208aa6d3ae8f229f958e43c9
Lucas Wargha <war18001@byui.edu>
2019-10-22 15:31:11 -0600

    team 6

--- web/week-6/team2.php
+++ web/week-6/team2.php
@@ -1,5 +1,20 @@
 <?php
+require("databaseLoader.php");
+$db = get_db();
+
 
 $book = $_POST['book'];
-echo $book;
-?>
+$chapter = $_POST['chapter'];
+$verse = $_POST['verse'];
+$content = $_POST['content'];
+$topic = $_POST['topic'];
+
+
+
+$query = 'INSERT INTO scripture(book, chapter, verse, content) VALUES(:book, :chapter, :verse, :content)';
+$statement = $db->prepare($query);
+$statement->bindValue(':book', $book);
+$statement->bindValue(':chapter', $chapter);
+$statement->bindValue(':verse', $verse);
+$statement->bindValue(':content', $content);
+$statement->execute();
