c5998c795b5f99b9a5e2bb136beaf7d1f2bc6de7
Lucas Wargha <war18001@byui.edu>
2019-10-22 15:36:09 -0600

    team 6

--- web/week-6/team2.php
+++ web/week-6/team2.php
@@ -11,10 +11,21 @@ $topic = $_POST['topic'];
 
 
 
-$query = 'INSERT INTO scripture(book, chapter, verse, content) VALUES(:book, :chapter, :verse, :content)';
+$query = 'INSERT INTO scripture (book, chapter, verse, content) VALUES(:book, :chapter, :verse, :content)';
 $statement = $db->prepare($query);
 $statement->bindValue(':book', $book);
 $statement->bindValue(':chapter', $chapter);
 $statement->bindValue(':verse', $verse);
 $statement->bindValue(':content', $content);
 $statement->execute();
+
+foreach ($topicIds as $topicId)
+	{
+		echo "ScriptureId: $scriptureId, topicId: $topicId";
+		// Again, first prepare the statement
+		$statement = $db->prepare('INSERT INTO scripture_topic(scriptureId, topicId) VALUES(:scriptureId, :topicId)');
+		// Then, bind the values
+		$statement->bindValue(':scriptureId', $scriptureId);
+		$statement->bindValue(':topicId', $topicId);
+		$statement->execute();
+	}
