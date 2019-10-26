3747c1d04331d2947010fb25b5b6b7cdddb3e0cd
Lucas Wargha <war18001@byui.edu>
2019-10-25 19:21:41 -0600

    Test

--- web/prj1/navTop.php
+++ web/prj1/navTop.php
@@ -13,16 +13,12 @@
       <span class="badge badge-danger">9+</span>
     </a>
     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="alertsDropdown">
-      <a class="dropdown-item" href="#">Action</a>
-      <a class="dropdown-item" href="#">Another action</a>
-      <div class="dropdown-divider"></div>
-      <a class="dropdown-item" href="#">Something else here</a>
     </div>
   </li>
   <li class="nav-item dropdown no-arrow mx-1">
     <a class="nav-link dropdown-toggle" href="#" id="messagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
       <i class="fas fa-envelope fa-fw"></i>
-      <span class="badge badge-danger">7</span>
+      <span class="badge badge-danger">2</span>
     </a>
     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="messagesDropdown">
       <a class="dropdown-item" href="#">Action</a>
