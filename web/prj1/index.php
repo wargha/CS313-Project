4dbc2e7eb99e7302a104812dc8b892c95fa48445
Lucas Wargha <war18001@byui.edu>
2019-10-25 19:17:20 -0600

    Test

--- web/prj1/index.php
+++ web/prj1/index.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <?php
 if (isset($_COOKIE['canLogin'])) {
-  echo "aqui";
   if ($_COOKIE['canLogin'] != "auth") {
     setcookie('canLogin', "not auth");
     header("Location: login.php");
@@ -95,21 +94,6 @@ u.id = 1
 
         <!-- Icon Cards-->
         <div class="row">
-          <div class="col-xl-3 col-sm-6 mb-3">
-            <div class="card text-white bg-primary o-hidden h-100">
-              <div class="card-body">
-                <div class="card-body-icon">
-                  <i class="fas fa-fw fa-comments"></i>
-                </div>
-                <div class="mr-5">Deals Inbox</div>
-              </div>
-              <a class="card-footer text-white clearfix small z-1" href="#">
-                <span class="float-left">View Details</span>
-                <span class="float-right">
-                  <i class="fas fa-angle-right"></i>
-                </span>
-              </a>
-            </div>
           </div>
           <div class="col-xl-3 col-sm-6 mb-3">
             <div class="card text-white bg-warning o-hidden h-100">
