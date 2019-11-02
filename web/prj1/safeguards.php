69b0bfee58f1a4c8a580705d0da39f77f12652b5
Lucas Wargha <war18001@byui.edu>
2019-11-02 10:44:06 -0600

    test

--- web/prj1/safeguards.php
+++ web/prj1/safeguards.php
@@ -51,29 +51,6 @@
   <div id="wrapper">
 
     <!-- Sidebar -->
-    <ul class="sidebar navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" href="index.php">
-          <i class="fas fa-fw fa-tachometer-alt"></i>
-          <span>Dashboard</span>
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="editProfile.php">
-          <i class="fas fa-fw fa-edit"></i>
-          <span>Edit Profile</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="recipes.php">
-          <i class="fas fa-fw fa-table"></i>
-          <span>Recipe Database</span></a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="#">
-          <i class="fas fa-fw fa-utensils"></i>
-          <span>Allergy Safeguards</span></a>
-      </li>
-    </ul>
 
     <div id="content-wrapper">
 
