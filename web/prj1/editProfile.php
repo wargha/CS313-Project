f4cc026a99a3569f53d8a08a8679c51f101e7845
Lucas Wargha <war18001@byui.edu>
2019-11-02 11:23:09 -0600

    test

--- web/prj1/editProfile.php
+++ web/prj1/editProfile.php
@@ -5,6 +5,13 @@ $db = get_db();
 
 if(isset($_POST['removeAllergy'])){
  echo $_POST['removeAllergy'];
+ $sql = 'DELETE FROM user_allergy WHERE allergy_id = :allergy_id AND user_id = :user_id ';
+        $user_id = $_COOKIE['user_id'];
+        $allergy_id = $_POST['removeAllergy'];
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindValue(':allergy_id', $allergy_id);
+        $stmt->bindValue(':user_id', $user_id);
+        $stmt->execute();
 }  
 ?>
 <html lang="en">
