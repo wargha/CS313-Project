b42e8eeca93f0dbdc5138f4a5ca551fc052100c8
Lucas Wargha <war18001@byui.edu>
2019-11-02 09:40:16 -0600

    test

--- web/prj1/index.php
+++ web/prj1/index.php
@@ -78,8 +78,29 @@ u.id = 1
   <div id="wrapper">
 
     <!-- Sidebar -->
-    <?php require_once('sidebar.php') ?>
-
+    <ul class="sidebar navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>Dashboard</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="editProfile.php">
+          <i class="fas fa-fw fa-edit"></i>
+          <span>Edit Profile</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="recipes.php">
+          <i class="fas fa-fw fa-table"></i>
+          <span>Recipe Database</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+          <i class="fas fa-fw fa-utensils"></i>
+          <span>Find Restaurants</span></a>
+      </li>
+    </ul>
     <div id="content-wrapper">
 
       <div class="container-fluid">
