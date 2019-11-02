e07fee031ab54b0feb0af0e6e273310358206255
Lucas Wargha <war18001@byui.edu>
2019-11-02 13:16:42 -0600

    test

--- web/prj1/recipes.php
+++ web/prj1/recipes.php
@@ -94,7 +94,7 @@ foreach ($db->query('SELECT
         <div class="card mb-3">
           <div class="card-header">
             <i class="fas fa-chart-area"></i>
-            Your Allergies</div>
+            Your Recipes</div>
             <div class="card-body d-flex flex-row justify-content-around">
           <?php
           // echo "HERE!!!";
@@ -111,6 +111,44 @@ foreach ($db->query('SELECT
        ?>
 
           </div>
+          
+        <div class="modal fade" id="modal-recipe-day" tabindex="-1" role="dialog" aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
+          <div class="modal-dialog modal-dialog-scrollable" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalScrollableTitle">Dairy Free Bread</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <p> 'In a large mixing bowl or the bowl of a standing mixer with a hook attachment, combine 2 cups of warm almond milk, 1/4 cup sugar, and 1 1/2 tablespoons yeast, stirring gently to dissolve. Let the mixture rest for 5 to 10 minutes, or until foamy (showing the yeast is active).
+
+
+                  Meanwhile, in a medium-large mixing bowl, combine 6 cups flour and 1 1/2 teaspoons salt.
+
+                  Add 1/4 cup oil to the yeast mixture. Gradually add the flour, about one cup at a time.
+
+                  Turn the dough out onto a lightly floured surface and knead it until the dough is elastic and firm.
+
+                  Place the dough in a lightly oiled bowl, cover the bowl with plastic wrap, and put it in a warm place for 1 hour, or until the dough is doubled in bulk.
+
+                  Preheat the oven to 350 F. Oil two 9x5 inch loaf pans.
+
+                  Punch down the dough, and turn out onto a dry surface, kneading the dough for 3 to 4 minutes. Divide the dough in half, shape each half into a loaf, and place them in the prepared loaf pans. Let the loaves rise in a warm place for 30 to 40 minutes more, or until the loaves have risen above the edges of the pans.
+
+                  Bake for 30 minutes or until golden brown.
+
+                  Remove the loaves from the oven and allow them to cool slightly in the pans before slicing and serving.'</p>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              </div>
+
+
+            </div>
+          </div>
+        </div>
         </div>
 
       </div>
